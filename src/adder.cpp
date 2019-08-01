522ee6b81cc3a4a83607be863aa7aa2381e1fc27
Zoltan Resi <resizoltan@live.com>
2019-08-01 13:31:18 +0200

    lots of changes

--- src/adder.cpp
+++ src/adder.cpp
@@ -7,26 +7,29 @@
 
 #include "beginner_tutorials/adder.h"
 
-Adder::Adder() {
-	//NumberOfCallsCallback = [](int64_t &dummy) {};
+Adder::Adder()
+{
+  // NumberOfCallsCallback = [](int64_t &dummy) {};
 }
 
-Adder::Adder(string id) :
-		Adder() {
-	this->id = id;
+Adder::Adder(string id) : Adder()
+{
+  this->id = id;
 }
 
-int64_t Adder::Add(int64_t a, int64_t b) {
-	call_count++;
-	NumberOfCallsCallback(call_count);
-	return static_cast<int64_t>(a) + static_cast<int64_t>(b);
+int64_t Adder::Add(int64_t a, int64_t b)
+{
+  call_count++;
+  NumberOfCallsCallback(call_count);
+  return static_cast<int64_t>(a) + static_cast<int64_t>(b);
 }
 
-string Adder::GetID() {
-	return id;
+string Adder::GetID()
+{
+  return id;
 }
 
 void Adder::AddHandler(std::function<void(const int&)> handler)
 {
-	NumberOfCallsCallback = handler;
+  NumberOfCallsCallback = handler;
 }
--- test/test_node.cpp
+++ test/test_node.cpp
@@ -10,39 +10,32 @@
 #include "beginner_tutorials/adder.h"
 #include "beginner_tutorials/AddTwoInts.h"
 
-
-
-
 TEST(AdderNode, commonValues)
 {
-	ros::NodeHandle nh;
-	ros::ServiceClient client = nh.serviceClient<beginner_tutorials::AddTwoInts>("add_two_ints");
-	beginner_tutorials::AddTwoInts srv;
-	srv.request.a = 1;
-	srv.request.b = 3;
-	ASSERT_TRUE(client.call(srv));
-	EXPECT_EQ(srv.response.sum, 4);
-
+  ros::NodeHandle nh;
+  ros::ServiceClient client = nh.serviceClient<beginner_tutorials::AddTwoInts>("add_two_ints");
+  beginner_tutorials::AddTwoInts srv;
+  srv.request.a = 1;
+  srv.request.b = 3;
+  ASSERT_TRUE(client.call(srv));
+  EXPECT_EQ(srv.response.sum, 4);
 }
 
 TEST(AdderNode, wrongResults)
 {
-	ros::NodeHandle nh;
-	ros::ServiceClient client = nh.serviceClient<beginner_tutorials::AddTwoInts>("add_two_ints");
-	beginner_tutorials::AddTwoInts srv;
-	srv.request.a = 1;
-	srv.request.b = 3;
-	ASSERT_TRUE(client.call(srv));
-	EXPECT_NE(srv.response.sum, 5);
+  ros::NodeHandle nh;
+  ros::ServiceClient client = nh.serviceClient<beginner_tutorials::AddTwoInts>("add_two_ints");
+  beginner_tutorials::AddTwoInts srv;
+  srv.request.a = 1;
+  srv.request.b = 3;
+  ASSERT_TRUE(client.call(srv));
+  EXPECT_NE(srv.response.sum, 5);
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-	testing::InitGoogleTest(&argc, argv);
-	ros::init(argc, argv, "adder_tester");
+  testing::InitGoogleTest(&argc, argv);
+  ros::init(argc, argv, "adder_tester");
 
-
-	return RUN_ALL_TESTS();
-
+  return RUN_ALL_TESTS();
 }
-
