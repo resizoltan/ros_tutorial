ce115cb575d574b331d5a3a81715587c229956c9
Zoltan Resi <resizoltan@live.com>
2019-08-02 16:21:24 +0200

    without catkin and pushd-popd

--- build_for_sonar.sh
+++ build_for_sonar.sh
@@ -1,10 +1,10 @@
 current_dir=$PWD
 output_dir_local="bw-output"
 output_dir_global="$PWD/$output_dir_local"
-pushd /root/catkin_ws
-catkin clean --build --yes
+cd /root/catkin_ws
+rm -rf build
 mkdir build
 cd build
 cmake ../src/beginner_tutorials
 build-wrapper-linux-x86-64 --out-dir $output_dir_global make
-popd
+cd /home/travis/build/kroshu/rosintro
