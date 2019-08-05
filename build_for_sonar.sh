df1f94aa2c1d439904a092b75e3eca53fea2bb54
Zoltan Resi <resizoltan@live.com>
2019-08-05 10:30:21 +0200

    check env path

--- build_for_sonar.sh
+++ build_for_sonar.sh
@@ -1,13 +1,14 @@
 current_dir=$PWD
 #output_dir_local="bw-output"
 #output_dir_global="$PWD/$output_dir_local"
+echo $PATH
 cd /root/catkin_ws
 rm -rf build
 mkdir build
 cd build
-cmake ../src/beginner_tutorials
+cmake ../src/rosintro
 build-wrapper-linux-x86-64 --out-dir "/root/catkin_ws/bw-output" make
-cd /root/catkin_ws/src/beginner_tutorials
+cd /root/catkin_ws/src/rosintro
 sonar-scanner
 cd $current_dir
 #cd /home/travis/build/kroshu/rosintro
