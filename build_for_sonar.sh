4ff1fadc17da503bc1632a317ad587fdb08a5f7f
Zoltan Resi <resizoltan@live.com>
2019-08-05 10:16:34 +0200

    trying with AFTER_SCRIPT

--- build_for_sonar.sh
+++ build_for_sonar.sh
@@ -1,10 +1,13 @@
 current_dir=$PWD
-output_dir_local="bw-output"
-output_dir_global="$PWD/$output_dir_local"
+#output_dir_local="bw-output"
+#output_dir_global="$PWD/$output_dir_local"
 cd /root/catkin_ws
 rm -rf build
 mkdir build
 cd build
 cmake ../src/beginner_tutorials
-build-wrapper-linux-x86-64 --out-dir $output_dir_global make
-cd /home/travis/build/kroshu/rosintro
+build-wrapper-linux-x86-64 --out-dir "/root/catkin_ws/bw-output" make
+cd /root/catkin_ws/src/beginner_tutorials
+sonar-scanner
+cd $current_dir
+#cd /home/travis/build/kroshu/rosintro
