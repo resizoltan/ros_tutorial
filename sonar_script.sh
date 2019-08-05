9796129b6a0dba62e142670c399a6b4dfe4b7784
Zoltan Resi <resizoltan@live.com>
2019-08-05 17:35:08 +0200

    docker hase name, docker cp used

--- sonar_script.sh
+++ sonar_script.sh
@@ -1,8 +1,2 @@
-# shellcheck source=industrial_ci/src/util.sh
-source "${ICI_SRC_PATH}/util.sh"
-# shellcheck source=industrial_ci/src/docker.sh
-source "${ICI_SRC_PATH}/docker.sh"
-# shellcheck source=industrial_ci/src/env.sh
-source "${ICI_SRC_PATH}/env.sh"
 # copy wrapper report to local filesystem
-docker_cp $cid:/root/catkin_ws/build-sonar/bw-output /home/travis/build/rosintro/bw-output
+docker cp ici_docker:/root/catkin_ws/build-sonar/bw-output /home/travis/build/rosintro/bw-output
