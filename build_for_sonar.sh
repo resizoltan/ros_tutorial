0d4395463402f86833cbc8c8ff06778ec5bc43d4
Zoltan Resi <resizoltan@live.com>
2019-08-05 10:45:36 +0200

    trying to find commands

--- build_for_sonar.sh
+++ build_for_sonar.sh
@@ -7,8 +7,10 @@ rm -rf build
 mkdir build
 cd build
 cmake ../src/rosintro
-build-wrapper-linux-x86-64 --out-dir "/root/catkin_ws/bw-output" make
+ls /home/travis/.sonar/cache/681277d525b6e4c5ed5e6c2a429caa64/build-wrapper-linux-x86/
+/home/travis/.sonar/cache/681277d525b6e4c5ed5e6c2a429caa64/build-wrapper-linux-x86/build-wrapper-linux-x86-64 --out-dir "/root/catkin_ws/bw-output" make
 cd /root/catkin_ws/src/rosintro
-sonar-scanner
+ls /home/travis/.sonarscanner/sonar-scanner-3.0.3.778/bin
+/home/travis/.sonarscanner/sonar-scanner-3.0.3.778/bin/sonar-scanner
 cd $current_dir
 #cd /home/travis/build/kroshu/rosintro
