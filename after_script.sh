fd77961d1fd795d4f261e5907c4c7f4dd746f716
Zoltan Resi <resizoltan@live.com>
2019-08-05 16:44:35 +0200

    docker_cp moved out of docker(i guess)

--- after_script.sh
+++ after_script.sh
@@ -9,14 +9,6 @@ cmake ../src/rosintro
 /root/sonar/build-wrapper-linux-x86/build-wrapper-linux-x86-64 --out-dir "/root/catkin_ws/build_sonar/bw-output" make
 # make tests
 make test
-# copy wrapper report to local filesystem
-# shellcheck source=industrial_ci/src/util.sh
-source "${ICI_SRC_PATH}/util.sh"
-# shellcheck source=industrial_ci/src/docker.sh
-source "${ICI_SRC_PATH}/docker.sh"
-# shellcheck source=industrial_ci/src/env.sh
-source "${ICI_SRC_PATH}/env.sh"
-docker_cp CONTAINER:/root/catkin_ws/build-sonar/bw-output /home/travis/build/rosintro/bw-output
 # return to working directory
 cd $working_dir
 
---
+++ sonar_script.sh
@@ -0,0 +1,2 @@
+# copy wrapper report to local filesystem
+docker_cp $cid:/root/catkin_ws/build-sonar/bw-output /home/travis/build/rosintro/bw-output
