e99060bbe89abdd2276f5e56380113f3cfb8ad84
Zoltan Resi <resizoltan@live.com>
2019-08-05 18:03:59 +0200

    copy src to diff folder before sonar

--- after_script.sh
+++ after_script.sh
@@ -9,6 +9,10 @@ cmake ../src/rosintro
 /root/sonar/build-wrapper-linux-x86/build-wrapper-linux-x86-64 --out-dir "/root/catkin_ws/build_sonar/bw-output" make
 # make tests
 make test
+# run sonar scanner
+cp /root/catkin_ws/src/rosintro /root/sonar/src/rosintro
+cd /root/sonar/src/rosintro
+/root/sonar/sonar-scanner-4.0.0.1744-linux/bin/sonar-scanner
 # return to working directory
 cd $working_dir
 
