aaeabe509020a41266614170cc1cf53f50a80a61
Zoltan Resi <resizoltan@live.com>
2019-08-06 11:38:50 +0200

    added name to container

--- before_script.sh
+++ before_script.sh
@@ -7,10 +7,10 @@ mkdir /root/sonar
 cd /root/sonar
 # install build wrapper for sonarqube c++
 wget https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip
-unzip build-wrapper-linux-x86.zip
+unzip -q build-wrapper-linux-x86.zip
 chmod +x build-wrapper-linux-x86/build-wrapper-linux-x86-64
 # install sonar-scanner
 wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.0.0.1744-linux.zip
-unzip sonar-scanner-cli-4.0.0.1744-linux.zip
+unzip -q sonar-scanner-cli-4.0.0.1744-linux.zip
 # return to working directory
 cd $working_dir
