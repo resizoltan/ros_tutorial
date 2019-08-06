3610251c390198d6553cdae073149a1dd03f4758
Zoltan Resi <resizoltan@live.com>
2019-08-06 13:23:24 +0200

    docker cp bw-putput to local

--- after_script.sh
+++ after_script.sh
@@ -9,11 +9,11 @@ cmake ../src/rosintro
 /root/sonar/build-wrapper-linux-x86/build-wrapper-linux-x86-64 --out-dir "/root/catkin_ws/build_sonar/bw-output" make
 # make tests
 make test
-# run sonar scanner
-mkdir /root/sonar/src
-cp -r /root/catkin_ws/src/rosintro /root/sonar/src/
-cd /root/sonar/src/rosintro
-/root/sonar/sonar-scanner-4.0.0.1744-linux/bin/sonar-scanner
+## run sonar scanner
+#mkdir /root/sonar/src
+#cp -r /root/catkin_ws/src/rosintro /root/sonar/src/
+#cd /root/sonar/src/rosintro
+#/root/sonar/sonar-scanner-4.0.0.1744-linux/bin/sonar-scanner
 # return to working directory
 cd $working_dir
 
--- sonar_script.sh
+++ sonar_script.sh
@@ -1,2 +1,4 @@
 # copy wrapper report to local filesystem
-docker cp ici_docker:/root/catkin_ws/build-sonar/bw-output /home/travis/build/rosintro/bw-output
+docker cp $CONTAINER_NAME:/root/catkin_ws/build-sonar/bw-output /home/travis/build/rosintro/bw-output
+# run sonar scanner
+sonar-scanner
