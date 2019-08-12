6e5b185d90596d2ae610baa881bd484d88294c48
Zoltan Resi <resizoltan@live.com>
2019-08-12 15:30:26 +0200

    writeable source

--- after_script.sh
+++ after_script.sh
@@ -14,14 +14,14 @@ make test
 #mkdir /root/sonar/src
 #cp -r /root/catkin_ws/src/rosintro /root/sonar/src/
 cd /root/catkin_ws/src/rosintro
-whoami
-lsattr
-ls -la
-chmod 777 /root/catkin_ws/src/rosintro
-lsattr
-ls -la
-fdisk -l
-cat /etc/fstab
+#whoami
+#lsattr
+#ls -la
+#chmod 777 /root/catkin_ws/src/rosintro
+#lsattr
+#ls -la
+#fdisk -l
+#cat /etc/fstab
 /root/sonar/sonar-scanner-4.0.0.1744-linux/bin/sonar-scanner
 # return to working directory
 cd $working_dir
